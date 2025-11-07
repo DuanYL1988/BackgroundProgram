a6659e1f21006f75b17429899ebc6e1fbc9ec62b
dylsweetdeath <dylsweetdeath@gmail.com>
2025-11-07 18:37:32 +0900

    测试执行原生SQL

--- Springboot/mysql/src/test/java/com/example/mysql/repository/TableInfoRepositoryTest.java
+++ Springboot/mysql/src/test/java/com/example/mysql/repository/TableInfoRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.example.mysql.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.annotation.Rollback;
 
 import com.example.mysql.dto.TableInfoDto;
@@ -16,6 +20,9 @@ class TableInfoRepositoryTest {
 
     @Autowired
     TableInfoRepository testRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     @Rollback
@@ -30,5 +37,24 @@ class TableInfoRepositoryTest {
         List<TableInfo> resultList = testRepository.selectByDto(condition);
         Assert.assertTrue(resultList.size() == searchCount);
 
+    }
+
+    @Test
+    public void testJdbcTemplate(){
+        String query = "SELECT DISTINCT NAME_CN,NAME,NAME_JP FROM FIREEMBLEM_HERO WHERE NAME = ?";
+        List<Object[]> queryResult = jdbcTemplate.query(query, new Object[]{"Lyn"}, new RowMapper<Object[]>() {
+            @Override
+            public Object[] mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Object[]{
+                        rs.getString("NAME_CN"),
+                        rs.getString("NAME"),
+                        rs.getString("NAME_JP")
+                };
+            }
+        });
+        for(Object[] row: queryResult) {
+            //String[] columns = (String[]) row;
+            System.out.println(row[0] + ";" + row[1] + ";" + row[2]);
+        }
     }
 }
